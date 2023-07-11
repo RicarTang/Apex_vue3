89446718069778320d90bf08aeb644655b1d9a69
Tang <357965604@qq.com>
2023-07-11 19:51:06 +0800

    修改index.html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,7 +20,35 @@ import Menu from '@/components/layout/Menu.vue'
 <style scoped>
 /* .common-layout {
 } */
+.el-container {
+    /* height: 100%; */
+    padding: 0px;
+    margin: 0px;
+    height: 100%;
+    widows: 100%;
+}
 
+.el-header,
+.el-footer {
+    background-color: #b3c0d1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+}
+
+.el-aside {
+    background-color: #d3dce6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+}
+
+.el-main {
+    background-color: #e9eef3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+}
 
 /* .el-menu {
     background-color: #d3dce6;
